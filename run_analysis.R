52e3451e689504b33fa85cdc62d57d4ae36ceb49
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-01-21 17:37:29 -0500

    fully functional

--- run_analysis.R
+++ run_analysis.R
@@ -5,6 +5,7 @@
 
 # create our data folder
 if (!file.exists("./data")) {
+    print("data folder not available, creating it now...")
     dir.create("./data")
 }
 
@@ -12,10 +13,12 @@ if (!file.exists("./data")) {
 f <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 # create temp file, unzip the file, then store as meaningful name (Dataset)
 if (!file.exists("./data/UCI HAR Dataset")){
+    print("Downloading and extracting dataset...")
     temp <- tempfile()
     download.file(f, destfile = temp)
     unzip(temp, exdir = "./data/")
     unlink(temp)
+    print("Create dataset in './data/UCI HAR Dataset'")
 }
 
 # function to read the files and appropriate columns
@@ -29,7 +32,7 @@ read_vals <- function(fname) {
     # grep column names which have '*-mean()*' and '*-std()* in them, boolean
     f1 <- grepl("(.-mean\\(\\).)", col_names)
     f2 <- grepl("(.std\\(\\).)", col_names)
-    idx <-as.logical(f1 + f2)
+    idx <- as.logical(f1 + f2)
     
     # make a subset of the column names based on the idx
     sub_col_names <- col_names[idx]
@@ -49,11 +52,36 @@ read_vals <- function(fname) {
 }
 
 # read training and testing sets
+print("Creating and merging test and training sets...")
 train <- read_vals("./data/UCI HAR Dataset/train/X_train.txt")
 test <- read_vals("./data/UCI HAR Dataset/test/X_test.txt")
 
 # stack them on top of each other
 df <- rbind(train, test)
 
-# celebrate
-str(df)
+# take care of the variable names. let's use some descriptive names to rename
+# these variables.  the variables come from the accelerometers and gyroscope. 
+# prefix 't' denotes time and prefix 'f' denotes frequency.
+
+var_names <- names(df)
+
+# suffix mean and std
+var_names <- sub("-mean\\(\\)", "_mean", var_names)
+var_names <- sub("-std\\(\\)", "_std", var_names)
+
+# replace spatial variables with '_{i}' for i={X, Y, z}
+var_names <- sub("-X", "_x", var_names)
+var_names <- sub("-Y", "_y", var_names)
+var_names <- sub("-Z", "_z", var_names)
+
+# replace the t and f time 'time_' and 'freq_'
+var_names <- sub("^f", "freq_", var_names)
+var_names <- sub("^t", "time_", var_names)
+
+# rename
+names(df) <- var_names
+
+# save the resulting dataset as csv file
+out_name <- "tidy_dataset.csv"
+write.table(df, out_name, sep = ",")
+print("Saved resulting tidy dataset in file 'tidy_dataset.csv'")
