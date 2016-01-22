80497854188a5e0474f4fc91f78d4795d8ac21d3
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-01-21 20:24:14 -0500

    fixed to contain subject and activity data too

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,9 @@
 ##### first things first, we get the data
-
 ##### note my .gitignore file specifies it will ignore files in the data folder
 ##### running this file on any machine should create the data and output
+
+# load necessary libraries
+library(dplyr)
 
 # create our data folder
 if (!file.exists("./data")) {
@@ -21,67 +23,61 @@ if (!file.exists("./data/UCI HAR Dataset")){
     print("Create dataset in './data/UCI HAR Dataset'")
 }
 
-# function to read the files and appropriate columns
-read_vals <- function(fname) {
-    # first load the "features.txt" file to get the colnames
-    col_names <- readLines("./data/UCI HAR Dataset/features.txt")
-    
-    # remove leading number from the name 
-    col_names <- sub("^([0-9]){1,3} ", "", col_names)
-    
-    # grep column names which have '*-mean()*' and '*-std()* in them, boolean
-    f1 <- grepl("(.-mean\\(\\).)", col_names)
-    f2 <- grepl("(.std\\(\\).)", col_names)
-    idx <- as.logical(f1 + f2)
-    
-    # make a subset of the column names based on the idx
-    sub_col_names <- col_names[idx]
-    
-    # initialize class of columns for later reading
-    col_classes <- rep("numeric", length(idx))
-    
-    for (i in 1:length(col_classes)) {
-        if (idx[i]==F) {
-            col_classes[i] = "NULL"
-        } 
-    }    
-    
-    df <- read.table(fname, col.names = col_names, colClasses = col_classes)
-    names(df) <- sub_col_names
-    return(df)
-}
+# read training and test data raw data 
+print("Merging training and test raw data...")
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+x <- merge(x_train, x_test, all = TRUE)
 
-# read training and testing sets
-print("Creating and merging test and training sets...")
-train <- read_vals("./data/UCI HAR Dataset/train/X_train.txt")
-test <- read_vals("./data/UCI HAR Dataset/test/X_test.txt")
+# read training and test activity data
+print("Merging training and testing activity data...")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+y <- rbind(y_train, y_test)
 
-# stack them on top of each other
-df <- rbind(train, test)
+# read training and testing subject data
+print("Merging training and test subject data...")
+s_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt") 
+s_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+s <- merge(s_train, s_test, all = TRUE)
 
-# take care of the variable names. let's use some descriptive names to rename
-# these variables.  the variables come from the accelerometers and gyroscope. 
-# prefix 't' denotes time and prefix 'f' denotes frequency.
+# get activity labels
+activity <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
 
-var_names <- names(df)
+# get feature names
+col_names <- read.table("./data/UCI HAR Dataset/features.txt")$V2
 
-# suffix mean and std
-var_names <- sub("-mean\\(\\)", "_mean", var_names)
-var_names <- sub("-std\\(\\)", "_std", var_names)
+# cleanup some of the feature names
+col_names <- sub("^t", "time_", col_names)
+col_names <- sub("^f", "freq_", col_names)
 
-# replace spatial variables with '_{i}' for i={X, Y, z}
-var_names <- sub("-X", "_x", var_names)
-var_names <- sub("-Y", "_y", var_names)
-var_names <- sub("-Z", "_z", var_names)
+# rename table columns
+colnames(x) <- col_names
+colnames(y) <- "activity"
+colnames(s) <- "subject"
 
-# replace the t and f time 'time_' and 'freq_'
-var_names <- sub("^f", "freq_", var_names)
-var_names <- sub("^t", "time_", var_names)
+# grep columns that have mean and stddev
+f1 <- grepl("(.-mean\\(\\).)", col_names)
+f2 <- grepl("(.std\\(\\).)", col_names)
+idx <- as.logical(f1 + f2)
+sub_col_names <- col_names[idx]
 
-# rename
-names(df) <- var_names
+# full table of measurements
+final <- cbind(s, y, x)
 
-# save the resulting dataset as csv file
-out_name <- "./tidy_dataset.txt"
-write.table(df, out_name, row.names = FALSE)
+# grab only mean and standard deviation
+final <- final[, sub_col_names]
+
+# tidy up the names
+final_names <- names(final)
+final_names <- sub("-mean\\(\\)", "_mean", final_names)
+final_names <- sub("-std\\(\\)", "_std", final_names)
+final_names <- sub("-X", "_x", final_names)
+final_names <- sub("-Y", "_y", final_names)
+final_names <- sub("-Z", "_z", final_names)
+
+# add new names
+names(final) <- final_names
+
+write.table(final, "./tidy_dataset.txt", row.names = FALSE)
 print("Saved resulting tidy dataset in file './tidy_dataset.txt'")
